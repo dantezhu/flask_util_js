7e880e7e95bfe39b560d01673ca3a5feb9c1ec6d
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-03-27 16:39:38 +0100

    Updated the generated flask_util.js

--- flask_util_js.py
+++ flask_util_js.py
@@ -47,62 +47,37 @@ FLASK_UTIL_JS_PATH = '/flask_util.js'
 FLASK_UTIL_JS_TPL_STRING = '''
 {%- autoescape false -%}
 var flask_util = function() {
-    var rule_map = {{ rule_map }};
-
-    function url_for(endpoint, params) {
-        if (!params) {
-            params = {};
-        }
-
-        if (!rule_map[endpoint]) {
-            throw('endpoint does not exist: ' + endpoint);
-        }
-
-        var rule = rule_map[endpoint];
-
-        var used_params = {};
-
-
-        var rex = /\<\s*(\w+:)*(\w+)\s*\>/ig;
-
-        var path = rule.replace(rex, function(_i, _0, _1) {
-            if (params.hasOwnProperty(_1)) {
-                used_params[_1] = params[_1];
-                return encodeURIComponent(params[_1]);
-            } else {
-                throw(_1 + ' does not exist in params');
-            }
-        });
-
-        var query_string = '';
-
-        for(var k in params) {
-            if (used_params.hasOwnProperty(k)) {
-                continue;
-            }
-
-            var v = params[k];
-            if(query_string.length > 0) {
-                query_string += '&';
-            }
-            query_string += encodeURIComponent(k)+'='+encodeURIComponent(v);
-        }
-
-        var url = path;
-        if (query_string.length > 0) {
-            url += '?'+query_string;
-        }
-
-        return url;
+  var rule_map = {{rule_map}};
+  function url_for(endpoint, params) {
+    if (!params) {params = {}}
+    if (!rule_map[endpoint]) {throw('endpoint does not exist: ' + endpoint)}
+    var rule = rule_map[endpoint];
+    var used_params = {};
+    var rex = /\<\s*(\w+:)*(\w+)\s*\>/ig;
+    var path = rule.replace(rex, function(_i, _0, _1) {
+      if (params.hasOwnProperty(_1)) {
+        used_params[_1] = params[_1];
+        return encodeURIComponent(params[_1]);
+      } else {
+        throw(_1 + ' does not exist in params');
+      }
+    });
+    var query_string = '';
+    for (var k in params) {
+      if (used_params.hasOwnProperty(k)) {continue}
+      var v = params[k];
+      if (query_string.length > 0) {query_string += '&'}
+      query_string += encodeURIComponent(k) + '=' + encodeURIComponent(v);
     }
-
-    return {
-        url_for: url_for,
-        rule_map: rule_map
-    }
+    var url = path;
+    if (query_string.length > 0) {url += '?' + query_string}
+    return url;
+  }
+  return {url_for: url_for, rule_map: rule_map}
 }();
 {%- endautoescape -%}
 '''
+
 
 class FlaskUtilJs(object):
     """FlaskUtilJs"""
